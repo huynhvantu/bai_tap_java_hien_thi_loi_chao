812e8e8c821c36797cc107016a06bbc953964481
huynhvantu <huynhvantunlp@gmail.com>
2019-06-22 08:42:23 +0700

    first commit

---
+++ hienthiloichao.java
@@ -0,0 +1,14 @@
+package hien_thi_loi_chao;
+
+import java.util.Scanner;
+
+public class hienthiloichao {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhap vao ten cua ban");
+		String ten = scanner.nextLine();
+		System.out.println("Xin chao "+ ten);
+	}
+
+}
